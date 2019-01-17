dc03590513af3aaf02bd49c736406fe702d0bec2
thareqmy <thareqmyha@gmail.com>
2019-01-17 22:09:49 +0700

    create step procedure for framebuffer component

--- square.c
+++ square.c
@@ -6,57 +6,52 @@
 #include <sys/mman.h>
 #include <sys/ioctl.h>
 
-int main()
-{
-    int fbfd = 0;
-    struct fb_var_screeninfo vinfo;
-    struct fb_fix_screeninfo finfo;
-    long int screensize = 0;
-    char *fbp = 0;
-    int x = 0, y = 0;
-    long int location = 0;
-
-    // Open the file for reading and writing
-    fbfd = open("/dev/fb0", O_RDWR);
-    if (fbfd == -1) {
-        perror("Error: cannot open framebuffer device");
-        exit(1);
-    }
-    printf("The framebuffer device was opened successfully.\n");
-
+void getScreenInformation(int fbfd, struct fb_var_screeninfo* vinfo, struct fb_fix_screeninfo* finfo) {
     // Get fixed screen information
-    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+    if (ioctl(fbfd, FBIOGET_FSCREENINFO, finfo) == -1) {
         perror("Error reading fixed information");
         exit(2);
     }
 
     // Get variable screen information
-    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, vinfo) == -1) {
         perror("Error reading variable information");
         exit(3);
     }
+}
 
-    printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
+void initializeFrame(long long* screensize, int* fbfd, struct fb_var_screeninfo* vinfo, struct fb_fix_screeninfo* finfo) {
+    //Initialize Frame Information from Hardware Memory
+
+    (*fbfd) = 0;
+    (*screensize) = 0;
+
+    // Open the file for reading and writing
+    (*fbfd) = open("/dev/fb0", O_RDWR);
+    if ((*fbfd) == -1) {
+        perror("Error: cannot open framebuffer device");
+        exit(1);
+    }
+    printf("The framebuffer device was opened successfully.\n");
+
+    getScreenInformation((*fbfd), vinfo, finfo);
+
+    printf("%dx%d, %dbpp\n", (*vinfo).xres, (*vinfo).yres, (*vinfo).bits_per_pixel);
 
     // Figure out the size of the screen in bytes
-    screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+    (*screensize) = (*vinfo).xres * (*vinfo).yres * (*vinfo).bits_per_pixel / 8;
 
-    // Map the device to memory
-    fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
-    if ((int)fbp == -1) {
-        perror("Error: failed to map framebuffer device to memory");
-        exit(4);
-    }
-    printf("The framebuffer device was mapped to memory successfully.\n");
+}
 
-    //THE PIXEL INPUT
+void paintPixel(char *fbp, long long screensize, struct fb_var_screeninfo vinfo, struct fb_fix_screeninfo finfo) {
+    //Procedure to paint the pixel for display
 
-    x = 100; y = 100;       // Where we are going to put the pixel
-
+    //Location
+    int x = 100, y = 100;
+    long int location = 0;
     // Figure out where in memory to put the pixel
-    for (y = 100; y < 300; y++)
+    for (y = 100; y < 300; y++) {
         for (x = 100; x < 300; x++) {
-
             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                        (y+vinfo.yoffset) * finfo.line_length;
 
@@ -65,6 +60,7 @@ int main()
                 *(fbp + location + 1) = 255;//15+(x-100)/2;     // A little green
                 *(fbp + location + 2) = 255;//200-(y-100)/5;    // A lot of red
                 *(fbp + location + 3) = 0;      // No transparency
+            
         //location += 4;
             } else  { //assume 16bpp
                 int b = 10;
@@ -73,9 +69,39 @@ int main()
                 unsigned short int t = r<<11 | g << 5 | b;
                 *((unsigned short int*)(fbp + location)) = t;
             }
-
         }
+    }
     munmap(fbp, screensize);
+
+}
+
+int main()
+{
+    //Frame Information
+    int fbfd;
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    long long screensize;
+
+
+    //Pixel Information
+    //R, G, B, and Transparency FORMAT
+    char *fbp = 0;
+
+    initializeFrame(&screensize, &fbfd, &vinfo, &finfo);
+
+
+    // Map the device to program
+    fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+    if ((int)fbp == -1) {
+        perror("Error: failed to map framebuffer device to memory");
+        exit(4);
+    }
+    printf("The framebuffer device was mapped to memory successfully.\n");
+
+    //Paint the pixel
+    paintPixel(fbp, screensize, vinfo, finfo);
+    
     close(fbfd);
     return 0;
 }
