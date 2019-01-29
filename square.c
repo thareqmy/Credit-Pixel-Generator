5d3e47eedf18d386377b561b359a938132ef7d3f
thareqyusuf <thareqmyha@gmail.com>
2019-01-29 23:38:52 +0700

    Add files via upload

--- square.c
+++ square.c
@@ -5,121 +5,146 @@
 #include <linux/fb.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
-#include <sys/kd.h>
-#include <string.h>
-#include "point.h"
-#include "color.h"
-#include "frame.h"
-
-void getScreenInformation();
-void initializeFrame();
-void pixelBG(int height, int width, Color BGColor, char** fbp);
-void paintPixel(char *fbp);
-
-void arrayPointToFBP(Point* points, int N, Color pixelColor, char** fbp); 
-
-
-//Global Frame Information
-int fbfd;
-struct fb_var_screeninfo vinfo;
-struct fb_fix_screeninfo finfo;
-long long screensize;
 
 int main()
 {
+    int fbfd = 0;
+    struct fb_var_screeninfo vinfo;
+    struct fb_fix_screeninfo finfo;
+    long int screensize = 0;
+    char *fbp = 0;
+    int x = 0, y = 0;
+    long int location = 0;
 
+    // Open the file for reading and writing
+    fbfd = open("/dev/fb0", O_RDWR);
+    if (fbfd == -1) {
+        perror("Error: cannot open framebuffer device");
+        exit(1);
+    }
+    printf("The framebuffer device was opened successfully.\n");
 
-    //Pixel Information
-    //R, G, B, and Transparency FORMAT
-    Frame currFrame;
+    // Get fixed screen information
+    if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo) == -1) {
+        perror("Error reading fixed information");
+        exit(2);
+    }
 
-    //int tty_fd = open("/dev/tty0", O_RDWR);
-    //ioctl(tty_fd,KDSETMODE,KD_GRAPHICS);
-    initializeFrame(&currFrame);
+    // Get variable screen information
+    if (ioctl(fbfd, FBIOGET_VSCREENINFO, &vinfo) == -1) {
+        perror("Error reading variable information");
+        exit(3);
+    }
 
+    printf("%dx%d, %dbpp\n", vinfo.xres, vinfo.yres, vinfo.bits_per_pixel);
 
-    // Map the device to program
-    currFrame.fbp = (char *)mmap(0, currFrame.screensize, PROT_READ | PROT_WRITE, MAP_SHARED, currFrame.fbfd, 0);
-    if ((int)currFrame.fbp == -1) {
+    // Figure out the size of the screen in bytes
+    screensize = vinfo.xres * vinfo.yres * vinfo.bits_per_pixel / 8;
+
+    // Map the device to memory
+    fbp = (char *)mmap(0, screensize, PROT_READ | PROT_WRITE, MAP_SHARED, fbfd, 0);
+    if ((int)fbp == -1) {
         perror("Error: failed to map framebuffer device to memory");
         exit(4);
     }
     printf("The framebuffer device was mapped to memory successfully.\n");
 
-    //Paint the pixel
+    x = 100; y = 100;       // Where we are going to put the pixel
 
-    paintPixel(fbp);
-    for (y = 100; y < 100 + height; y++) {
-        for (x = 100; x < 100 + width; x++) {
+    // Figure out where in memory to put the pixel
+    for (y = 25; y <750; y++)
+        for (x = 25; x < 1500; x++) {
+
             location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
                        (y+vinfo.yoffset) * finfo.line_length;
 
             if (vinfo.bits_per_pixel == 32) {
-                *((*fbp) + location) = BGColor.b;// Blue
-                *((*fbp) + location + 1) = BGColor.g;// Green
-                *((*fbp) + location + 2) = BGColor.r;// Red
-                *((*fbp) + location + 3) = 0;      // No transparency
-
-
-void paintPixel(char *fbp) {
-    //Procedure to paint the pixel for display
-
-    Color BGColor = {100, 100, 100};
-    Color pixelColor = {0, 0, 0};
-    Point apoints[625];
-    Point bpoints[625];
-
-    int count = 0;
-    for (int i = 5; i < 25; i++) {
-        for (int j = 0; j < 5; j++) {
-            apoints[count] = {i, j};
-            count++;
-        }
-    }
-    for (int i = 0; i < 5; i++) {
-        for (int j = 5; j < 15; j++) {
-            apoints[count] = {i, j};
-            count++;
-        }
-    }    
-    for (int i = 5; i < 25; i++) {
-        for (int j = 0; j < 5; j++) {
-            apoints[count] = {i, j};
-            count++;
-        }
-    }
-
-    pixelBG(200, 200, BGColor, &fbp);
-    arrayPointToFBP(points, 12, pixelColor, &fbp);
-
-
-
-    munmap(fbp, screensize);
-
-}
-void arrayPointToFBP(Point* points, int N, Color pixelColor, char** fbp) {
-    //Convert array of point to device FBP 
-
-    long int location;
-
-    for (int i = 0; i < N; ++i) {
-        location = (points[i].x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
-                       (points[i].y+vinfo.yoffset) * finfo.line_length;
-
-        if (vinfo.bits_per_pixel == 32) {
-                *((*fbp) + location) = pixelColor.b;// Blue
-                *((*fbp) + location + 1) = pixelColor.g;// Green
-                *((*fbp) + location + 2) = pixelColor.r;// Red
-                *((*fbp) + location + 3) = 0;      // No transparency
-            
+                *(fbp + location) = 0;        // Some blue
+                *(fbp + location + 1) = 0;     // A little green
+                *(fbp + location + 2) = 0;    // A lot of red
+                *(fbp + location + 3) = 0;      // No transparency
         //location += 4;
-            } else  {
-                //ni kaga penting gausah dimengerti
+            } else  { //assume 16bpp
                 int b = 10;
-                int g = (points[i].x-100)/6;     // A little green
-                int r = 31-(points[i].y-100)/16;    // A lot of red
+                int g = (x-100)/6;     // A little green
+                int r = 31-(y-100)/16;    // A lot of red
                 unsigned short int t = r<<11 | g << 5 | b;
-                *((unsigned short int*)((*fbp) + location)) = t;
+                *((unsigned short int*)(fbp + location)) = t;
             }
+
+        }
+    FILE *fp;
+    
+
+    int curr = 0;
+
+    while (curr < 150) {
+
+        for (y = 25; y <750; y++)
+        for (x = 25; x < 1500; x++) {
+
+            location = (x+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+                       (y+vinfo.yoffset) * finfo.line_length;
+
+            if (vinfo.bits_per_pixel == 32) {
+                *(fbp + location) = 0;        // Some blue
+                *(fbp + location + 1) = 0;     // A little green
+                *(fbp + location + 2) = 0;    // A lot of red
+                *(fbp + location + 3) = 0;      // No transparency
+        //location += 4;
+            } else  { //assume 16bpp
+                int b = 10;
+                int g = (x-100)/6;     // A little green
+                int r = 31-(y-100)/16;    // A lot of red
+                unsigned short int t = r<<11 | g << 5 | b;
+                *((unsigned short int*)(fbp + location)) = t;
+            }
+
+        }
+        fp = fopen("name.txt", "r");
+    while(!feof(fp)) {
+    	
+    	fscanf(fp, "%d", &x);
+    	fscanf(fp, "%d", &y);
+
+    	int BASE = 4;
+
+    	for (int i = x * BASE + 25; i < (x + 1) * BASE + 25; i++) {
+    		for (int j = y * BASE + 25 + curr; j < (y + 1) * BASE + 25 + curr; j++) {
+    			
+                if((i > 25 && i < 750) && (j > 25 && j < 1500)) {
+
+                location = (i+vinfo.xoffset) * (vinfo.bits_per_pixel/8) +
+                       (j+vinfo.yoffset) * finfo.line_length;
+
+            if (vinfo.bits_per_pixel == 32) {
+                *(fbp + location) = 0;        // Some blue
+                *(fbp + location + 1) = rand() % 255;     // A little green
+                *(fbp + location + 2) = rand() % 255;    // A lot of red
+                *(fbp + location + 3) = rand() % 255;      // No transparency
+        //location += 4;
+            } else  { //assume 16bpp
+                int b = 10;
+                int g = (x-100)/6;     // A little green
+                int r = 31-(y-100)/16;    // A lot of red
+                unsigned short int t = r<<11 | g << 5 | b;
+                *((unsigned short int*)(fbp + location)) = t;
+            }
+    		}
+        }
+    	}
+        
+
+    	
     }
-} 
+    curr+=20;
+    usleep(1000000);
+    }
+
+    fclose(fp);
+
+    
+    munmap(fbp, screensize);
+    close(fbfd);
+    return 0;
+}
